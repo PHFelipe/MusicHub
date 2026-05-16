0d9d6badf9b06fb2de8c59614ec30f22042b7e80
PHFelipe <paulo.academico01@gmail.com>
2026-05-16 16:12:25 -0300

    Create Catalogo

---
+++ src/main/java/com/musichub/Catalogo.java
@@ -0,0 +1,25 @@
+package com.musichub;
+
+import com.musichub.midia.Midia;
+
+import java.util.Map;
+
+public class Catalogo implements ServicoStreaming{
+
+    private Map<String,Midia> Midia;
+
+    @Override
+    public void adicionarMidia() {
+
+    }
+
+    @Override
+    public void removerMidia() {
+
+    }
+
+    @Override
+    public void BuscarMidia() {
+
+    }
+}
