bb813551f2d0971eefcdfb5c22a63a70cb415592
PHFelipe <paulo.academico01@gmail.com>
2026-05-17 14:48:43 -0300

    Implement the logic of basic functions

--- src/main/java/com/musichub/Catalogo.java
+++ src/main/java/com/musichub/Catalogo.java
@@ -2,24 +2,35 @@ package com.musichub;
 
 import com.musichub.midia.Midia;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Catalogo implements ServicoStreaming{
 
-    private Map<String,Midia> Midia;
+    private List<Midia> catalogoMidias;
 
-    @Override
-    public void adicionarMidia() {
-
+    public Catalogo() {
+        this.catalogoMidias = new ArrayList<>();
     }
 
     @Override
-    public void removerMidia() {
-
+    public void adicionarMidia(Midia midia) {
+        catalogoMidias.add(midia);
     }
 
     @Override
-    public void BuscarMidia() {
+    public boolean removerMidia(String titulo) {
+        return catalogoMidias.removeIf(p -> p.getTitulo().equalsIgnoreCase(titulo));
+    }
 
+    //Permite a busca por Artista ou por Titulo
+    @Override
+    public List<Midia> BuscarMidia(String termo) {
+        String t = termo.toLowerCase();
+        return catalogoMidias.stream().filter(
+                midia -> midia.getTitulo().toLowerCase().contains(t)
+                || midia.getArtista().toLowerCase().contains(t)
+        ).collect(Collectors.toList());
     }
 }
--- src/main/java/com/musichub/Playlist.java
+++ src/main/java/com/musichub/Playlist.java
@@ -1,4 +1,67 @@
 package com.musichub;
 
+import com.musichub.midia.Midia;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+    private int IDUsuario;
+    private String nomePlaylist;
+    private List<Midia> midias;
+
+    public Playlist(int IDUsuario, String nomePlaylist) {
+        this.IDUsuario = IDUsuario;
+        this.midias = new ArrayList<>();
+        this.nomePlaylist = nomePlaylist;
+    }
+
+    public String getNomePlaylist() { return nomePlaylist; }
+
+    public void setNomePlaylist(String nomePlaylist) { this.nomePlaylist = nomePlaylist; }
+
+    public int getIDUsuario() { return IDUsuario; }
+
+    public void setIDUsuario(int IDUsuario) { this.IDUsuario = IDUsuario; }
+
+    public boolean removerMidia (String titulo){
+        return midias.removeIf(m -> m.getTitulo().equalsIgnoreCase(titulo));
+    }
+
+    public String getDuracaoPlaylist() {
+
+        int totalHoras = 0;
+        int totalMinutos = 0;
+        int totalSegundos = 0;
+
+        for (Midia midia : midias) {
+
+            String duracao = midia.getDuracao();
+            String[] partes = duracao.split(":");
+
+            int horas = Integer.parseInt(partes[0]);
+            int minutos = Integer.parseInt(partes[1]);
+            int segundos = Integer.parseInt(partes[2]);
+
+            totalHoras += horas;
+            totalMinutos += minutos;
+            totalSegundos += segundos;
+        }
+
+        totalMinutos += totalSegundos / 60;
+        totalSegundos = totalSegundos % 60;
+        totalHoras += totalMinutos / 60;
+        totalMinutos = totalMinutos % 60;
+
+        return String.format(
+                "%02d:%02d:%02d",
+                totalHoras,
+                totalMinutos,
+                totalSegundos
+        );
+    }
+    public int getTotalFaixas() { return midias.size(); }
+    public List<Midia> getMidias() { return midias; }
+
+    public void insertMidia(Midia midia){ this.midias.add(midia); }
 }
